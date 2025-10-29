2ec378c662416a93a8ea1d54203c25910cd75446
arlgnv <lagunov.artamon@gmail.com>
2025-10-29 12:37:53 +0300

    feat: add basic structure

---
+++ prettier.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+  singleQuote: true,
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './app/globals.css',
+};
+
+export default config;
