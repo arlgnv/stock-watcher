81d4b54b4603e5b514fd99d6e68efab31f153355
arlgnv <lagunov.artamon@gmail.com>
2025-11-05 16:53:41 +0300

    feat: add typing for event payloads

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,4 +1,19 @@
-import { Inngest } from 'inngest';
+import { Inngest, EventSchemas } from 'inngest';
+
+// The type argument of the 'fromRecord' method should be a type alias, not an interface.
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+type Events = {
+  'app/user.created': {
+    data: {
+      email: string;
+      name: string;
+      country: string;
+      investmentGoals: string;
+      riskTolerance: string;
+      preferredIndustry: string;
+    };
+  };
+};
 
 export const inngest = new Inngest({
   id: 'signalist',
@@ -7,4 +22,5 @@ export const inngest = new Inngest({
       apiKey: process.env.GEMINI_API_KEY,
     },
   },
+  schemas: new EventSchemas().fromRecord<Events>(),
 });
