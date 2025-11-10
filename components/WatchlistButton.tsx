675ed461d9fea5df379195930db5fdb5755ae559
arlgnv <lagunov.artamon@gmail.com>
2025-11-10 12:15:57 +0300

    refactor: use existing component for button

--- components/WatchlistButton.tsx
+++ components/WatchlistButton.tsx
@@ -2,6 +2,8 @@
 
 import { useMemo, useState } from 'react';
 
+import { Button } from './ui/button';
+
 function WatchlistButton({
   symbol,
   isInWatchlist,
@@ -13,62 +15,30 @@ function WatchlistButton({
 
   const label = useMemo(() => {
     if (type === 'icon') return added ? '' : '';
+
     return added ? 'Remove from Watchlist' : 'Add to Watchlist';
   }, [added, type]);
 
   const handleClick = () => {
     const next = !added;
+
     setAdded(next);
     onWatchlistChange?.(symbol, next);
   };
 
-  if (type === 'icon') {
-    return (
-      <button
-        title={
-          added
-            ? `Remove ${symbol} from watchlist`
-            : `Add ${symbol} to watchlist`
-        }
-        aria-label={
-          added
-            ? `Remove ${symbol} from watchlist`
-            : `Add ${symbol} to watchlist`
-        }
-        className={`watchlist-icon-btn ${added ? 'watchlist-icon-added' : ''}`}
-        onClick={handleClick}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill={added ? '#FACC15' : 'none'}
-          stroke="#FACC15"
-          strokeWidth="1.5"
-          className="watchlist-star"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.385a.563.563 0 00-.182-.557L3.04 10.385a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345l2.125-5.111z"
-          />
-        </svg>
-      </button>
-    );
-  }
-
   return (
-    <button
-      className={`watchlist-btn ${added ? 'watchlist-remove' : ''}`}
+    <Button
+      className={`watchlist-btn ${added ? 'watchlist-remove' : 'yellow-btn'}`}
       onClick={handleClick}
     >
       {showTrashIcon && added ? (
         <svg
+          className="mr-2 h-5 w-5"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="mr-2 h-5 w-5"
         >
           <path
             strokeLinecap="round"
@@ -78,7 +48,7 @@ function WatchlistButton({
         </svg>
       ) : null}
       <span>{label}</span>
-    </button>
+    </Button>
   );
 }
 
