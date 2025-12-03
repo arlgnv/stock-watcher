f8ff2772fdce51d27cbe2042e934674123b86d70
alagunoff <lagunov.artamon@gmail.com>
2025-12-03 09:42:38 +0300

    refactor: make "sendDailyMarketNewsEmail" inngest function non-step function

--- inngest/functions/sendDailyMarketNewsEmail.ts
+++ inngest/functions/sendDailyMarketNewsEmail.ts
@@ -8,13 +8,11 @@ import inngest from '../client';
 const sendDailyMarketNewsEmail = inngest.createFunction(
   { id: 'send-daily-market-news-email' },
   [{ event: 'daily_market_news.prepared' }],
-  async ({ step, event }) => {
-    await step.run('send-email', async () => {
-      await nodemailer.sendMail({
-        to: event.data.userEmail,
-        subject: `Today's market news summary - ${dayjs().format('dddd, MMMM D, YYYY')}`,
-        html: createDailyMarketNewsEmail(event.data.marketNews),
-      });
+  async ({ event }) => {
+    await nodemailer.sendMail({
+      to: event.data.userEmail,
+      subject: `Today's market news summary - ${dayjs().format('dddd, MMMM D, YYYY')}`,
+      html: createDailyMarketNewsEmail(event.data.marketNews),
     });
   },
 );
