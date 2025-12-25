48fb5ec18206c6fc33837fefa88769997cb82030
arlgnv <arlgnv@outlook.com>
2025-12-25 13:08:21 +0300

    refactor: set dev mode explicitly

--- inngest/client.ts
+++ inngest/client.ts
@@ -5,6 +5,7 @@ import type { EventSchemas } from './types';
 const client = new Inngest({
   id: 'signalist',
   schemas: new InngestEventSchemas().fromRecord<EventSchemas>(),
+  isDev: false,
 });
 
 export default client;
