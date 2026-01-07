2446a60111a8da9e8e40869744520105a62fa75a
arlgnv <arlgnv@outlook.com>
2026-01-07 14:03:53 +0300

    build: update dependencies

--- knip.config.ts
+++ knip.config.ts
@@ -3,6 +3,9 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
   ignore: ['supabase/types.ts'],
   ignoreDependencies: ['postcss'],
+  commitlint: {
+    config: 'commitlint.config.mts',
+  },
   'lint-staged': {
     config: 'lint-staged.config.mts',
   },
